068b80bddcf868f83d81d55cd52a6cc72425b1e4
ilm <ilm@ubuntu>
2015-11-04 07:35:04 -0800

    My Linux version

--- src/javagittest/JavaGitTest.java
+++ src/javagittest/JavaGitTest.java
@@ -17,6 +17,7 @@ public class JavaGitTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("This is the good one");
     }
     
 }
